d1a840e6c21d2a472b3dfbb3518413b0604a51e2
mhbahmani <mhbahmani79@gmail.com>
2020-01-10 22:19:33 +0330

    implement sending news to subscribers

--- apps/notification/celery.py
+++ apps/notification/celery.py
@@ -1,6 +1,7 @@
 from celery import shared_task
+from django.core.mail import EmailMultiAlternatives
 
 
 @shared_task(name="notification")
-def send_email():
-    pass
+def send_email(message: EmailMultiAlternatives):
+    message.send()
--- apps/notification/models.py
+++ apps/notification/models.py
@@ -1,6 +1,8 @@
 import markdown
 from django.contrib.auth.models import User
-from django.core.mail import send_mail
+from django.template.loader import render_to_string
+
+from apps.notification.celery import *
 from django.db import models
 
 # Create your models here.
@@ -25,6 +27,7 @@ class Subscriber(models.Model):
 
 
 class EmailText(models.Model):
+    subject = models.CharField(null=False, max_length=100)
     text = models.TextField(null=False)
     html = models.TextField(editable=False)
 
@@ -34,3 +37,16 @@ class EmailText(models.Model):
     def save(self, force_insert=False, force_update=False, using=None,
              update_fields=None):
         self.html = markdown.markdown(self.text)
+        subscribers = Subscriber.objects.all()
+
+        context = {
+            'subject': self.subject,
+            'context': self.text
+        }
+        email_html_message = render_to_string('email/notification.html', context)
+        email_plaintext_message = render_to_string('email/notification.txt', context)
+        for index in range(start=0, stop=subscribers.count() + 1, step=20):
+            message = EmailMultiAlternatives(subject=self.subject, from_email="datadays.sharif@gmail.com",
+                                             cc=subscribers, body=email_plaintext_message)
+            message.attach_alternative(email_html_message, "text/html")
+            send_email.apply_async(message)
